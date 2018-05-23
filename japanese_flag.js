35084d4d2c3c21659cd283b2e1d503d2943d4a48
LilPacy <revivedtomorrow@gmail.com>
2018-05-23 20:51:38 +0900

    日の丸描いてみた

---
+++ japanese_flag.js
@@ -0,0 +1,7 @@
+function setup() { // 最初に1回だけ実行される
+  createCanvas(300,300); // ここでwidth,height変数に値が代入される
+  background(255);
+  stroke(200,0,0); // 円や線の色を決める,第4引数にアルファ値を取れる
+  fill(200,0,0); // ellipseの中の色を決める,第4引数にアルファ値を取れる
+  ellipse(width/2,height/2,100,100);
+}
